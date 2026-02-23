003dc2ab63e51291689bba34d334f5a1aab905df
Luigi Marrandino <luigi.marrandino95@gmail.com>
2026-02-23 21:48:14 +0100

    Added validation to CarToUpdate model

--- src/main/java/bartra/world/carsalesrentalsystem/models/cars/CarToUpdateRequest.java
+++ src/main/java/bartra/world/carsalesrentalsystem/models/cars/CarToUpdateRequest.java
@@ -1,8 +1,11 @@
 package bartra.world.carsalesrentalsystem.models.cars;
 
+import jakarta.validation.constraints.Min;
+
 public record CarToUpdateRequest(
         String make,
         String model,
+        @Min(1886)
         int year,
         double currentPrice,
         String plate)  {
